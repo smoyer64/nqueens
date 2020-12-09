989b3dfcb2d282c68ac72624ab4c1bb1b5a02ef8
Steve Moyer <smoyer1@selesy.com>
2020-12-09 11:54:50 -0500

    Adds test display names

--- app/src/test/java/com/selesy/nqueens/solvers/RuleTests.java
+++ app/src/test/java/com/selesy/nqueens/solvers/RuleTests.java
@@ -2,10 +2,13 @@ package com.selesy.nqueens.solvers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Rule")
 public class RuleTests {
 
+    @DisplayName("String toString()")
     @Test
     void testToString() {
         assertEquals("Diagonal", Rule.DIAGONAL.toString());
