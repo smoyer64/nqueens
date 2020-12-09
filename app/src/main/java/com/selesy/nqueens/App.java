6ac4089f20f084d407d15d392f78110b5c734ef3
Steve Moyer <smoyer1@selesy.com>
2020-12-08 22:09:22 -0500

    Changes main to exercise backtracking solver

--- app/src/main/java/com/selesy/nqueens/App.java
+++ app/src/main/java/com/selesy/nqueens/App.java
@@ -3,19 +3,31 @@
  */
 package com.selesy.nqueens;
 
-import static org.fusesource.jansi.Ansi.*;
-import static org.fusesource.jansi.Ansi.Color.*;
+import java.util.stream.IntStream;
 
-import org.fusesource.jansi.AnsiConsole;
+import com.selesy.nqueens.solvers.Solver;
+import com.selesy.nqueens.solvers.backtracking.BacktrackingSolver;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        AnsiConsole.systemInstall();
-        System.out.println(ansi().fg(RED).bg(WHITE).a(new App().getGreeting()).reset());
-        AnsiConsole.systemUninstall();
+        // AnsiConsole.systemInstall();
+        // System.out.println(ansi().fg(RED).bg(WHITE).a(new
+        // App().getGreeting()).reset());
+        // AnsiConsole.systemUninstall();
+        (new App()).run();
+    }
+
+    void run() {
+        IntStream.range(1, 27)
+                 .mapToObj(this::solve)
+                 .forEach(System.out::println);
+    }
+
+    String solve(int size) {
+        Solver solver = new BacktrackingSolver(size);
+        return solver.solve()
+                     .map(s -> String.format("%2d - %s", size, s.toString()))
+                     .orElse(String.format("%2d - No solution", size));
     }
 }
