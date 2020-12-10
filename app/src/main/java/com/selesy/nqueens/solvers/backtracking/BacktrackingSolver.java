7f9703e1491afc5fff333359766406b25e89d71a
Steve Moyer <smoyer1@selesy.com>
2020-12-09 20:01:18 -0500

    Refactors BacktrackingSolver state machine and adds apidocs.
    Resolves #2

--- app/src/main/java/com/selesy/nqueens/solvers/backtracking/BacktrackingSolver.java
+++ app/src/main/java/com/selesy/nqueens/solvers/backtracking/BacktrackingSolver.java
@@ -32,67 +32,83 @@ public class BacktrackingSolver implements Solver {
      * @return an Optional containing the solution if one is found.
      */
     public Optional<List<Square>> solve() {
-        char file = 0;
-        int rank = 0;
+        // this square is being used to move around the chessboard
+        // (essentially a file/rank pair).
+        Square pos = new Square(0, 0);
 
         do {
-            Square s = new Square(file, rank);
-
-            // if there's a valid square in this file, fill it and start
-            // at rank 0 on the next file
-            if (board.isValid(s)) {
-                board.putDown(s);
-
-                // if there's a queen in each file then we're done
-                if (board.getQueens()
-                         .size() >= board.size()) {
+            // if there's a valid square at pos, place a queen and
+            // continue with rank zero of the next file. If all
+            // queens have been placed, then break.
+            if (board.isValid(pos)) {
+                if (place(pos)) {
                     break;
                 }
-
-                // otherwise start at the lowest rank of the next file
-                file++;
-                rank = 0;
                 continue;
             }
 
             // try the next square (forward)
-            rank++;
+            forward(pos);
 
             // at the last rank for the file then (recursively) backtrack
-            if (rank >= board.size()) {
-                Square bt = backtrack(board.pickUp());
-                file = (char) bt.getFile();
-                rank = bt.getRank();
+            if (pos.getRank() >= board.size()) {
+                pos = backtrack(board.pickUp());
             }
 
             // after backtracking as far as possible, the file will be
             // zero (a) and the rank will be equal to the size of the
             // board so no additional positions are possible.
-        } while (rank < board.size());
+        } while (pos.getRank() < board.size());
 
         return Optional.of(board.getQueens())
                        .map(o -> o.size() == 0 ? null : o);
     }
 
-    /**
+    /*
      * Recursively backtracks through the already placed queens trying to find one
      * that can still have its rank increased without exceeding the boundaries of
      * the board. If the square's file is zero, no more recursion is possible so
      * that square is returned with an invalid rank (indicating there's no
      * solution).
      * 
-     * @param square
-     *                   the next candidate square.
+     * @param pos the next candidate square.
+     * 
      * @return the next valid square to try.
      */
-    Square backtrack(Square square) {
+    Square backtrack(Square pos) {
         // If the square is itself in the highest rank, try the next one
-        if (square.getRank() >= board.size() - 1 && square.getFile() > 0) {
+        if (pos.getRank() >= board.size() - 1 && pos.getFile() > 0) {
             return backtrack(board.pickUp());
         }
 
         // Once a available square is found, increase its rank
-        square.setRank(square.getRank() + 1);
-        return square;
+        pos.setRank(pos.getRank() + 1);
+        return pos;
+    }
+
+    /*
+     * Moves forward to the next square on the chessboard.
+     * 
+     * @param pos the current position on the chessboard.
+     */
+    void forward(Square pos) {
+        pos.setRank(pos.getRank() + 1);
+    }
+
+    /*
+     * Places a queen on the chessboard and continues to the first rank of the next
+     * file. If the puzzle is solved, returns true otherwise false.
+     * 
+     * @param pos the current position on the chessboard.
+     * 
+     * @return whether the puzzle is complete.
+     */
+    boolean place(Square pos) {
+        board.putDown(new Square(pos.getFile(), pos.getRank()));
+        pos.setFile(pos.getFile() + 1);
+        pos.setRank(0);
+
+        return board.size() == board.getQueens()
+                                    .size();
     }
 }
